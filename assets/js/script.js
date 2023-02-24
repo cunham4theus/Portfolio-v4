51d1ab577197d9190f5612b0c206dfa7c35c4e5d
cunham4theus <cunhamatheus.m4@gmail.com>
2023-02-24 12:24:04 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,149 @@
+function html() {
+  let number = document.getElementById('number-html');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 90) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 22);
+}
+
+html();
+
+function css() {
+  let number = document.getElementById('number-css');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 90) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 22);
+}
+
+css();
+
+function js() {
+  let number = document.getElementById('number-js');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 50) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 36);
+}
+
+js();
+
+function sass() {
+  let number = document.getElementById('number-sass');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 95) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 18);
+}
+
+sass();
+
+function figma() {
+  let number = document.getElementById('number-figma');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 70) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 30);
+}
+
+figma();
+
+function bootstrap() {
+  let number = document.getElementById('number-bootstrap');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 65) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 28);
+}
+
+bootstrap();
+
+function wordpress() {
+  let number = document.getElementById('number-wordpress');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 80) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 25);
+}
+
+wordpress();
+
+function elementor() {
+  let number = document.getElementById('number-elementor');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 80) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 25);
+}
+
+elementor();
+
+function react() {
+  let number = document.getElementById('number-react');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 30) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 45);
+}
+
+react();
+
+function git() {
+  let number = document.getElementById('number-git');
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 50) {
+      clearInterval();
+    } else {
+      counter += 1;
+      number.innerHTML = counter + '%';
+    }
+  }, 36);
+}
+
+git();
